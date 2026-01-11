e78173bcbf5738a7ae06faa524a7567d7c038a4e
abxijth <abhijithrpillai231@gmail.com>
2026-01-11 22:50:24 +0530

    commit

--- Task-05/melofi-frontend/src/pages/Login/Login.jsx
+++ Task-05/melofi-frontend/src/pages/Login/Login.jsx
@@ -2,10 +2,47 @@ import InputField from "../../components/InputField.jsx";
 import Button from "../../components/Button.jsx"
 import Logo from "../../components/Logo.jsx"
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
+import { useState } from "react";
 
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+
+
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    try {
+
+      const response = await fetch("http://127.0.0.1:5000/api/login", {method: 'POST', headers: {'Content-Type': 'application/json',}, body : JSON.stringify(data),});
+
+      const result = await response.json();
+
+      if (response.ok) {
+        console.log("Login Success");
+        navigate("/dashboard");
+      
+      } else {
+        console.error("Login Failed");
+     } 
+
+    } catch (error) {
+        console.error("Backend Down");
+    }
+
+
+  }
+
+
 
 	return (
 		<>
@@ -13,10 +50,14 @@ const Login = () => {
 			<div className="login-container">
 
 				<Logo className="login-page-logo"/>
-				<InputField type="email" name="email" id="email" className="login-page-input-field" placeholder="Enter your email"/>
-				<InputField type="password" name="password" id="password" className="login-page-input-field" placeholder="Enter your password"/>
-				<Link to="/dashboard"><Button text="Login" type="submit" className="login-btn"/></Link>
-				<p className="forgot-password"><u>forgot password?</u></p>
+        
+        <form className="login-form" onSubmit={handleSubmit}>
+  				<InputField type="email" name="email" id="email" className="login-page-input-field" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+	  			<InputField type="password" name="password" id="password" className="login-page-input-field" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+		  		<Button text="Login" type="submit" className="login-btn"/>
+        </form>
+
+			  	<p className="forgot-password"><u>forgot password?</u></p>
 				<p>New to MeloFi? <Link to="/signup"><u>Create an account</u></Link></p>
 
 			</div>
